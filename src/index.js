e4c58f9c8385fecf767960c54d4850c5195fa8a5
jinwangchina <jin.wang.china@gmail.com>
2016-11-07 17:34:44 +1100

    daily commit

--- src/index.js
+++ src/index.js
@@ -1,46 +1,96 @@
 
-export const X_TYPE_ACTION = "X_TYPE_ACTION";
+export const X_TYPE_ASYNC_STATUS = "X_UPDATE_ASYNC_STATUS";
+export const X_STATE_ASYNC = "x_async_status";
+export const X_STATE_VALUE_ASYNC_RUNNING = "x_async_running";
+export const X_STATE_VALUE_ASYNC_DONE = "x_async_done";
 
-export default function reduxXAction( extraArgument ) {
+const isXAction = ( action ) => {
+    return action.xAction != null;
+};
+
+const updateAsyncStatus = ( dispatch, asyncStatus ) => {
+    let action = {
+        type: X_TYPE_ASYNC_STATUS
+    };
+    if ( typeof asyncStatus === 'string' ) {
+        action = {
+            ...action,
+            xAction: {
+                xStateName: X_STATE_ASYNC,
+                xStateData: asyncStatus
+            }
+        };
+
+    } else {
+        action = {
+            ...action,
+            ...asyncStatus
+        };
+    }
+    dispatch( action );
+};
+
+const handleAsync = ( dispatch, xAction ) => {
+    if ( typeof xAction.xAsync === 'function' ) {
+        updateAsyncStatus( dispatch, xAction.xAsyncRunningAction != null ? xAction.xAsyncRunningAction : X_STATE_VALUE_ASYNC_RUNNING );
+        let promise = xAction.xAsync();
+        promise.then( ( result ) => {
+            xAction.xData = result;
+            updateAsyncStatus( dispatch, xAction.xAsyncDoneAction != null ? xAction.xAsyncDoneAction : X_STATE_VALUE_ASYNC_DONE );
+            dispatch( action );
+        } ).catch( ( error ) => {
+            xAction.xData = error;
+            xAction.xError = true;
+            updateAsyncStatus( dispatch, xAction.xAsyncDoneAction != null ? xAction.xAsyncDoneAction : X_STATE_VALUE_ASYNC_DONE );
+            dispatch( xAction );
+        } );
+    } else {
+        xAction.xData = xAction.xAsync;
+        dispatch( xAction );
+    }
+}
+
+export class XReducer {
+    create() {
+        return ( state = {}, action = null ) => {
+            if ( isXAction( action ) ) {
+                let xAction = action.xAction;
+                let newState = {};
+                if ( typeof xAction.xSync === 'function' ) {
+                    newState = xAction.xSync( state, action );
+                } else if ( xAction.xStateName != null ) {
+                    newState[ xAction.xStateName ] = xAction.xStateData;
+                }
+                return {
+                    ...state,
+                    ...newState
+                };
+            }
+            return state;
+        };
+    }
+}
+
+export const createXReducer = () => {
+    return new XReducer().create();
+};
+
+const createXActionMiddleware = ( args ) => {
     return ( store ) => ( next ) => ( action ) => {
         if ( isXAction( action ) ) {
-            if ( typeof action.xAsync === 'function' ) {
-                let promise = action.xAsync();
-                promise.then( ( result ) => {
-                    action.xData = result;
-                    next( action );
-                } );
+            let xAction = action.xAction;
+            if ( xAction.xAsync != null ) {
+                handleAsync( next, xAction );
             }
         }
         return next( action );
     };
 };
+const reduxXAction = createXActionMiddleware();
+reduxXAction.withExtraArgument = createXActionMiddleware;
+export default reduxXAction;
 
-export function reduxXReducer() {
-    return ( state = {}, action = null ) => {
-        if ( isXAction( action ) ) {
-            let newState = {};
-            if ( typeof action.xSync === 'function' ) {
-                newState = action.xSync( state, action );
-            } else if ( action.xState != null ) {
-                newState[ action.xState ] = action.xData;
-            }
-            return {
-                ...state,
-                ...newState
-            };
-        }
-        return state;
-    };
-};
 
-export const createXAction = ( action ) => {
-    return {
-        xType: X_TYPE_ACTION,
-        ...action
-    };
-};
 
-const isXAction = ( action ) => {
-    return action.xType === X_TYPE_ACTION;
-}
+
+
